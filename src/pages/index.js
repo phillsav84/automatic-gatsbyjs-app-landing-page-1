93e19109dc6f16fd12c94c7dec29ec7461e9859f
phillsav84 <31317703+phillsav84@users.noreply.github.com>
2019-07-09 21:28:36 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -183,6 +183,29 @@ const IndexPage = ({ data }) => (
             })}
           </div>
 
+    <section id="contact">
+        <div className="inner">
+            <section>
+                <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+
           <footer>
             <p className="footerText">
               Made by{" "}
